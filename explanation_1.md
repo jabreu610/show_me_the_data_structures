1a59ca3d6e1fd3943cf0aaaabdd31d99e9de08d9
Jordan Abreu <jabreu610@gmail.com>
2023-01-09 18:11:21 -0500

    Implements Problem 1

---
+++ explanation_1.md
@@ -0,0 +1,13 @@
+# Explanation 1
+
+## Rationale
+For the LRU cache exercise, I decided to leverage Python's [OrderedDict](https://docs.python.org/3.11/library/collections.html#collections.OrderedDict). OrderedDict instances keep track of insertion order and provide two helpful methods when implementing a simple LRU cache:
+
+1. popitem returns and removes a key value pair, passing `False` as the argument will ensure entities are popped from left
+2. move_to_end accepts an existing key as an argument and moves it, and its paired value, to the right most position
+
+## Runtime Analysis
+
+1. LRU_Cache's `get` method has a time complexity of `O(1)` and space complexity of `O(1)`. Retrieving a value by key from an OrderedDict is a constant time operation. No additional storage is allocated in the execution of this method.
+2. LRU_Cache's `set` method has a time complextity of `O(1)` and a space complexity of `O(1)`. Setting a value on an OrderedDict is a constant time operation. In the event the cache is full, removing the oldest accessed entry is also a constant time operation. No additional storage is allocated in the execution of this method.
+
---
+++ problem_1.py
@@ -0,0 +1,75 @@
+from collections import OrderedDict
+
+
+class LRU_Cache:
+    def __init__(self, capacity=5):
+        self.capacity = capacity
+        self.data = OrderedDict()
+
+    def __repr__(self) -> str:
+        return self.data.__repr__()
+
+    def __cache_is_full(self):
+        return len(self.data) >= self.capacity
+
+    def __used(self, key):
+        self.data.move_to_end(key)
+
+    def get(self, key):
+        if key in self.data:
+            self.__used(key)
+            return self.data[key]
+        return -1
+
+    def set(self, key, value):
+        if key not in self.data and self.__cache_is_full():
+            self.data.popitem(False)
+        self.data[key] = value
+        self.__used(key)
+
+# Test Case 1
+
+
+def eviction():
+    """
+    Tests proper eviction of cached data when adding entries at capacity
+    """
+    cache = LRU_Cache(5)
+    for entry in range(1, 6):
+        cache.set(entry, entry)
+    cache.get(1)
+    cache.get(3)
+    cache.set(6, 6)
+    cache.set(7, 7)
+    return cache
+
+# Test Case 2
+
+
+def cache_miss():
+    """
+    Tests retrieving a value from an empty cache ie cache miss
+    """
+    cache = LRU_Cache(5)
+    return cache.get(1)
+
+# Test Case 3
+
+
+def cache_key_collision():
+    """
+    Tests setting values to existing keys, expected behavior: value should be overwritten and cache entry should be considered "used"
+    """
+    cache = LRU_Cache(5)
+    for entry in range(1, 6):
+        cache.set(entry, entry)
+    cache.set(1, 'I was overwritten')
+    return cache
+
+
+print(eviction())
+# OrderedDict([(5, 5), (1, 1), (3, 3), (6, 6), (7, 7)])
+print(cache_miss())
+# -1
+print(cache_key_collision())
+# OrderedDict([(2, 2), (3, 3), (4, 4), (5, 5), (1, 'I was overwritten')])
